ea01fa946bd0f8c74ad58750042c6a01dada6bef
krish-bhanushali <bhanushali123krish@gmail.com>
2021-05-14 17:32:28 +0530

    added testfile, instruction comments and requirements.txt

--- Text To Audio Convertor/TextToAudio.py
+++ Text To Audio Convertor/TextToAudio.py
@@ -1,12 +1,16 @@
-  
+#pip install -r requirements.txt
 import pyttsx3
 import PyPDF2
-book=open('IOT-Beginer.pdf', 'rb')
+
+#add pdf files in the project directory
+
+book=open('pdf-test.pdf', 'rb')
 pdfreader=PyPDF2.PdfFileReader(book)
 pages=pdfreader.numPages
 #print(pages)
 speaker=pyttsx3.init()
 page=pdfreader.getPage(0)
 text=page.extractText()
+print('saying')
 speaker.say(text)
 speaker.runAndWait()
